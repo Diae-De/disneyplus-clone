ce4c3a0ef9916cbbca0bed61cd50f839fa4dbcd1
Dyae Pr <diaeprsup@gmail.com>
2022-03-20 19:16:22 +0100

    change commit

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -36,6 +36,7 @@ function ImgSlider() {
 export default ImgSlider
 
 const Carousel = styled(Slider)`
+margin-top:20px;
     .slick-list{
         overflow:visible;
     }
@@ -51,8 +52,16 @@ const Carousel = styled(Slider)`
         color:white;
     }
 
-    button{
+    & > button{
+        opacity:0;
+        height:100%;
+        width:5vw;
         z-index:1;
+
+        &:hover{
+            opacity:1;
+            transition:opacity 0.2s ease 0s;
+        }
     }
 `
 
